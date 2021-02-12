96513c4ec1112625a83bfc6abd7e99ff4708fbca
nattharee <penquin501@gmail.com>
2021-02-12 15:39:17 +0700

    take limit in json

--- src/index.js
+++ src/index.js
@@ -20,7 +20,8 @@ const initDb = require("./env/parceldb.js");
 const initAmqp = require("./env/amqp");
 
 moment.locale("th");
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use(express.static("public"));
 
 const parcelServices = require("./services/parcelService.js");
