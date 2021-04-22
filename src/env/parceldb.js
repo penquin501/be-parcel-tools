0b1fb85f4a3ff42734d92df87a280cee46350d5a
nattharee <penquin501@gmail.com>
2021-04-22 22:55:09 +0700

    fix connect db

--- src/env/parceldb.js
+++ src/env/parceldb.js
@@ -1,14 +1,10 @@
 var mysql = require('mysql');
 var connection = mysql.createPool({
     connectionLimit : 10,
-    // host : process.env.MYSQL_HOST,
-    // user : process.env.MYSQL_USERNAME,
-    // password : process.env.MYSQL_PASSWORD,
-    // database : process.env.MYSQL_DATABASE_NAME,
-    host : '159.65.137.79',
-    user : 'parceldev',
-    password : '123456',
-    database : 'parcel',
+    host : process.env.MYSQL_HOST,
+    user : process.env.MYSQL_USERNAME,
+    password : process.env.MYSQL_PASSWORD,
+    database : process.env.MYSQL_DATABASE_NAME,
     debug : false,
     timezone : '+07:00'
 });
